00968c151db29bbede17f9d4e98e2b2fd2467028
WRITTICK_SENGUPTA <68491183+writticksengupta@users.noreply.github.com>
2022-06-15 12:04:42 +0530

    Add files via upload

---
+++ project1/terraformmodule/vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+#variable "AMIS" {
+ # type = map(string)
+  #default = {
+   # us-east-1 = "ami-0b0ea68c435eb488d"
+    #us-east-2 = "ami-05803413c51f242b7"
+    #us-west-1 = "ami-0454207e5367abf01"
+  #}
+#}
