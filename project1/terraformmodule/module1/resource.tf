cbffca4e2ec6d8b0d0a977ed17a75b886e3e7bc0
WRITTICK_SENGUPTA <68491183+writticksengupta@users.noreply.github.com>
2022-06-16 00:12:12 +0530

    Update resource.tf

--- project1/terraformmodule/module1/resource.tf
+++ project1/terraformmodule/module1/resource.tf
@@ -5,15 +5,16 @@ resource "aws_vpc" "vpc" {
   Name = "project vpc"
  }
 }
-resource "aws_subnet" "public_subnet" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block =  "10.0.1.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone = "us-east-2a"
-  tags = {
-    Name = "Public Subnet"
-  }
-}
+# (Optional) Used for NAT gateway and public ec2 instance
+# resource "aws_subnet" "public_subnet" {
+#   vpc_id = "${aws_vpc.vpc.id}"
+#   cidr_block =  "10.0.1.0/24"
+#   map_public_ip_on_launch = "true"
+#   availability_zone = "us-east-2a"
+#   tags = {
+#     Name = "Public Subnet"
+#   }
+# }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id = "${aws_vpc.vpc.id}"
@@ -25,24 +26,24 @@ resource "aws_subnet" "private_subnet" {
     Name = "Private Subnet"
   }
 }
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  tags = {
-    Name = "igw"
-  }
-}
-resource "aws_route_table" "public_rt" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw.id}"
-  }
-  tags = {
-    "Name" = "Public Route Table"
-  }
-}
-  resource "aws_route_table_association" "public" {
-    subnet_id = "${aws_subnet.public_subnet.id}"
-    route_table_id = "${aws_route_table.public_rt.id}"
-}
+# (Optional) For public ec2 instance and subnet
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = "${aws_vpc.vpc.id}"
+#   tags = {
+#     Name = "igw"
+#   }
+# }
+# resource "aws_route_table" "public_rt" {
+#   vpc_id = "${aws_vpc.vpc.id}"
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = "${aws_internet_gateway.igw.id}"
+#   }
+#   tags = {
+#     "Name" = "Public Route Table"
+#   }
+# }
+#   resource "aws_route_table_association" "public" {
+#     subnet_id = "${aws_subnet.public_subnet.id}"
+#     route_table_id = "${aws_route_table.public_rt.id}"
+# }
