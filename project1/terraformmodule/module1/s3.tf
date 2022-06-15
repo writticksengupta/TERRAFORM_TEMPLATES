2ef77db6baca54fdb2f129a6d8f31ad007af8299
WRITTICK_SENGUPTA <68491183+writticksengupta@users.noreply.github.com>
2022-06-16 00:15:53 +0530

    Update s3.tf

--- project1/terraformmodule/module1/s3.tf
+++ project1/terraformmodule/module1/s3.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "bucket1" {
     bucket = "writticks3backupbucket1"
     acl = "private"
+ # As my state files are going to be stored in s3 bucket, I chose to keep the bucket private
 
     versioning {
       enabled = true
