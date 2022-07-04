8c032ff6cb3b716f54c3297176667a2afb68e902
WRITTICK_SENGUPTA <68491183+writticksengupta@users.noreply.github.com>
2022-07-03 23:54:15 -0700

    Create main.tf

---
+++ EBS/main.tf
@@ -0,0 +1,12 @@
+resource "aws_ebs_volume" "ebs-volume-1" {
+availability_zone = "eu-west-1a"
+size = 20
+type = "gp2"
+tags {
+Name = "extra volume data"
+}
+}
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+device_name = "/dev/xvdh"
+volume_id = aws_ebs_volume.ebs-volume-1.id
+instance_id = aws_instance.example.id
