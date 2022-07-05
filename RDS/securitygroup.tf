4f61b7519d451f494193a566924b5728534e9c57
WRITTICK_SENGUPTA <68491183+writticksengupta@users.noreply.github.com>
2022-07-05 04:23:09 -0700

    Create securitygroup.tf

---
+++ RDS/securitygroup.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "example-instance" {
+  vpc_id =aws_vpc.main.id
+  name = "allow-ssh"
+  description = "security group that allows ssh and all egress traffic"
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+  }
+ingress {
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+tags {
+  Name = "example-instance
+ }
+}
+resource "aws_security_group" "allow-mariadb" {
+  vpc_id =aws_vpc.main.id
+  name = "allow-mariadb"
+ egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+    self = true
+  }
+ingress {
+  from_port = 3306
+  to_port = 3306
+  protocol = "tcp"
+  security_groups = aws_security_groups.example-instance.id  #allowing access from our example instance
+}
+tags {
+  Name = "allow mariadb"
+ }
+}
