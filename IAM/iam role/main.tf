248de9b9042175a90b8c5ea53406616ff44c9d37
WRITTICK_SENGUPTA <68491183+writticksengupta@users.noreply.github.com>
2022-07-14 05:16:18 -0700

    Create main.tf

---
+++ IAM/iam role/main.tf
@@ -0,0 +1,22 @@
+ resource "aws_iam_role" "s3-mybucket-role" {
+   name = "s3-mybucket-role"
+   assume_role_policy=<<EOF
+   {
+     "version":2012-10-17",
+     "statement":[
+     {
+     "Action":"sts:AssumeRole",
+     "Principal":{
+     "Service":"ec2.amazonaws.com"
+     }
+     "Effect":"Allow",
+     "Sid":""
+     }
+     ]
+     }
+     EOF
+     }
+     resource "aws_iam_instance_profile" "s3-mybucket-role-instanceprofile"{
+     name = "s3-mybucket-role"
+     roles = aws_iam_role.s3-mybucket-role.name
+     }
